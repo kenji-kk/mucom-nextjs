7d47162511cc07a540655362df97e29221fa53b8
kenji <kenjix12x22@gmail.com>
2022-04-24 00:27:17 +0900

    環境変数をnext用に変更

--- api/client.ts
+++ api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-  baseURL: `${process.env.REACT_APP_HOST}:8080`
+  baseURL: `${process.env.NEXT_PUBLIC_HOST}:8080`
 })
 
 export default client
