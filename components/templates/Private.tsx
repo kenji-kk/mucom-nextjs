d7642148e43e065a9bf0f7d80b872d2b2c5bfbdf
kenji <kenjix12x22@gmail.com>
2022-04-24 11:27:07 +0900

    Privateコンポーネント作成

---
+++ components/templates/Private.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Router from 'next/router'
+import Cookies from 'js-cookie';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store'
+import { authSlice } from '../../store/auth';
+import { SignupPage } from '../../components/pages/SignupPage';
+
+
+export const Private = ({children}: {children: React.ReactElement}) => {
+  const dispatch = useDispatch()
+  const auth = useSelector((state: RootState) => state.auth)
+  const {loading, isSignedIn, jwt} = auth.auth
+
+  if (!loading) {
+    if (isSignedIn) {
+      return children
+    } else {
+      if (Cookies.get("_access_token")) {
+        dispatch(authSlice.actions.authSetJWT(
+          Cookies.get("_access_token")
+        ))
+        dispatch(authSlice.actions.authSetIsSignedIn(
+          true
+        ))
+        return children
+      } else {
+        return <SignupPage />
+      }
+    }
+  } else {
+    return <></>
+  }
+}
+
